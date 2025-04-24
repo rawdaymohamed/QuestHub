3ef4053654cd4c89229519613c4b0989e2e5fa30
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-04-24 03:19:16 +0200

    Feat: Add register and api/token/

---
+++ backend/core/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework.serializers import ModelSerializer
+from rest_framework.permissions import AllowAny
+
+
+# Serializer for registration
+class RegisterSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username", "email", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+# View for registration
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
